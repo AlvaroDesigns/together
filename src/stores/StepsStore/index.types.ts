ff42d3db819f6aade701f82e4fef732c1eb9ac57
Alvaro Saiz <alvaro.saiz.bonilla@nnespana.es>
2024-11-06 13:29:14 +0100

    Fix: remove stores

--- src/stores/StepsStore/index.types.ts
+++
@@ -1,7 +0,0 @@
-import { LoadType } from '@/types/stores';
-
-export interface StepState {
-  step: LoadType;
-  // eslint-disable-next-line no-unused-vars
-  set: (step: LoadType) => void;
-}
--- src/stores/index.tsx
+++
@@ -1,6 +0,0 @@
-import { useDataStore } from './DataStore';
-import * as StoreTypes from './DataStore/index.types';
-import { useStepStore } from './StepsStore';
-import * as StepStoreTypes from './StepsStore/index.types';
-
-export { StepStoreTypes, StoreTypes, useDataStore, useStepStore };
