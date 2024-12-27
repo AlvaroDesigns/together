d903b75bd0a8641f732c5dbbeb2ef495fac2e053
Alvaro saiz <hello@alvarodesigns.com>
2024-12-27 19:26:35 +0100

    Feat: Add Loader Hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,5 @@
 import { format } from "@formkit/tempo";
-import { Card, CardBody, Image } from "@nextui-org/react";
+import { Card, CardBody, Image, Skeleton } from "@nextui-org/react";
 
 export default function Hero({
   startDate,
@@ -25,13 +25,34 @@ export default function Hero({
         className="w-full h-[300px] col-span-12 sm:col-span-7"
       >
         <CardBody className="absolute z-30 flex flex-col items-center w-full mt-9 top-1">
-          <p className="mt-1 text-white">
-            {startDate && format(new Date(startDate), "DD MMM YYYY")}{" "}
-            {endDate && `a ${format(new Date(endDate), "DD MMM YYYY")}`}
-          </p>
-          <h1 className="font-sans text-4xl font-bold text-white">{title}</h1>
-          <p className="mt-1 text-white">{days} Días · A tu aire flexible</p>
+          {loading ? (
+            <>
+              <Skeleton className="w-2/5 mt-2 rounded-lg">
+                <div className="w-2/5 h-3 rounded-lg bg-default-300" />
+              </Skeleton>
+              <Skeleton className="w-4/5 mt-2 rounded-lg">
+                <div className="w-2/5 h-10 rounded-lg bg-default-300" />
+              </Skeleton>
+              <Skeleton className="w-3/5 mt-2 rounded-lg">
+                <div className="w-2/5 h-3 rounded-lg bg-default-300" />
+              </Skeleton>
+            </>
+          ) : (
+            <>
+              <p className="mt-1 text-white">
+                {startDate && format(new Date(startDate), "DD MMM YYYY")}{" "}
+                {endDate && `a ${format(new Date(endDate), "DD MMM YYYY")}`}
+              </p>
+              <h1 className="font-sans text-4xl font-bold text-white">
+                {title}
+              </h1>
+              <p className="mt-1 text-white">
+                {days} Días · A tu aire flexible
+              </p>
+            </>
+          )}
         </CardBody>
+
         <Image
           removeWrapper
           isLoading={loading}
